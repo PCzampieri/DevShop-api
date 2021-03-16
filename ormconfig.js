19107a53d1125372e67c7603f7bece85074a1b82
Paulo Cezar Lima Zampieri <pczampieri@hotmail.com>
2021-03-16 09:36:31 -0300

    fix, ormconfig dist2

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,33 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config()
 
+const extra = {
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
+
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'development' ? false : true,
+  extra: process.env.NODE_ENV === 'development' ? {} : extra,
   logging: true,
-  entities: ['dist/**/*.entity.js'],
-  migrations: ['dist/migrations/**/*.js'],
+  entities: [
+    process.env.NODE_ENV === 'development'
+      ? 'src/**/*.entity.ts'
+      : 'dist/**/*.entity.js'
+  ],
+  migrations: [
+    process.env.NODE_ENV === 'development'
+      ? 'src/migrations/**/*.ts'
+      : 'dist/migrations/**/*.js'
+  ],
   cli: {
-    entitiesDir: 'src/**',
-    migrationsDir: 'src/migrations/'
+    entitiesDir: process.env.NODE_ENV === 'development' ? 'src/**' : 'dist/**',
+    migrationsDir:
+      process.env.NODE_ENV === 'development'
+        ? 'src/migrations/'
+        : 'dist/migrations/'
   }
 }
